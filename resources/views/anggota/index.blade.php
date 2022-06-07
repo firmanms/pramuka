a45aa29c8ec29092b3811290144e6738d0d3cbe8
firmanms <firmanmuhamadsahidin@gmail.com>
2022-06-07 11:00:54 +0700

    menambah show

--- resources/views/anggota/index.blade.php
+++ resources/views/anggota/index.blade.php
@@ -5,8 +5,8 @@
         <div class="card card-body border-0 shadow table-wrapper table-responsive">
             {{-- <h2 class="mb-4 h5">{{ __('Users') }}</h2> --}}
             <div class="card-header border-bottom d-flex align-items-center justify-content-between"><h2 class="fs-5 fw-bold mb-0">Anggota</h2>
-                
-                
+
+
             </div>
             <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
                 @csrf
@@ -18,7 +18,7 @@
                 <div class="input-group-append">
                   <button class="btn btn-success" type="submit">Unggah</button><a class="btn btn-warning" href="{{ route('export') }}">Unduh</a>
                 </div>
-              </div>           
+              </div>
                 {{-- <a class="btn btn-warning" href="{{ route('export') }}">Export User Data</a> --}}
             </form><br>
             {{-- <p class="text-info mb-0">Sample table page</p> --}}
@@ -30,24 +30,24 @@
             @can('anggota-create')
                 <a href="{{ route('anggotas.create') }}" class="btn btn-success" style="width: 11%;">Tambah Anggota</a>
                 @endcan
-            
+
             <table class="table table-hover anggota_datatable">
                 <thead>
                     <tr>
                         <th class="border-gray-200">id</th>
-                        <th class="border-gray-200">nik</th> 
+                        <th class="border-gray-200">nik</th>
                         <th class="border-gray-200">nama</th>
-                        <th class="border-gray-200">tingkat</th> 
-                        <th class="border-gray-200">ranting</th>                        
+                        <th class="border-gray-200">tingkat</th>
+                        <th class="border-gray-200">ranting</th>
                         <th class="border-gray-200">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
-                    
+
                 </tbody>
             </table>
             {{-- {!! $users->render() !!} --}}
-            
+
         </div>
     </div>
 
@@ -62,14 +62,17 @@
             <div class="modal-body">
                 <table border="0" width="100%">
                     <tr>
-                        <td>NIK</td><td>: <font id="nik"></font></td>                        
+                        <td>NIK</td><td>: <font id="nik"></font></td>
                     </tr>
-                    <tr>                        
+                    <tr>
                         <td>Nama</td><td>: <font id="nama"></font></td>
                     </tr>
-                </table>                
+                    <tr>
+                        <td>tingkat</td><td>: <font id="tingkat"></font></td>
+                    </tr>
+                </table>
             </div>
-            <div class="modal-footer">                
+            <div class="modal-footer">
                 <button type="button" class="btn btn-link text-gray ms-auto" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
@@ -123,13 +126,13 @@ function hapus(e) {
                     if (data.type === 'success') {
                         $('.anggota_datatable').DataTable().ajax.reload();
                         Swal.fire({
-                          
+
                           icon: 'success',
                           title: 'Data telah dihapus',
                           showConfirmButton: false,
                           timer: 1500
                         })
-                        
+
                     } else if (data.type === 'danger') {
                         swal("Error deleting!", "Try again", "error");
                     }
@@ -152,16 +155,16 @@ function showProject(id)
             type: "GET",
             success: function(response) {
                 let anggota = response.anggota;
-                
+
                 $("#nik").html(anggota.nik);
                 $("#nama").html(anggota.nama);
-                $("#view-modal").modal('show'); 
-                 
+                $("#view-modal").modal('show');
+
             },
             error: function(response) {
                 console.log(response.responseJSON)
             }
         });
-    }   
+    }
 </script>
 @endsection
